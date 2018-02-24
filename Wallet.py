9e29f62fa9164d610939dba197cade4d1f726568
Jim <jmdelaney8@gmail.com>
2018-02-24 10:02:47 -0800

    Housekeeping. Removing giles no longer needded.

--- Wallet.py
+++
@@ -1,8 +0,0 @@
-"""Wallet module pulls heavily from BIP32Node (of which it inherets)"""
-from pycoin.key.BIP32Node import BIP32Node
-
-
-class Wallet(BIP32Node):
-    def do_nothing(self):
-        """Stand in so class works"""
-        return
